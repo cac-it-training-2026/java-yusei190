7a29a3c8c0eaf696f49048e2a9acd9e0e99f0544
ykamimu <ykamimu@C010423151.zipangu.cac.co.jp>
2026-04-16 10:50:34 +0900

    Question01_2完了

--- java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
@@ -1,27 +1,21 @@
 package basic.question01;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 /**
  * 第4章 変数
  */
 
 public class Question01_2 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int applePrice = 100;
+		String stationery = "鉛筆";
+		int bananaPrice = applePrice;
+		String writingUtensil = stationery;
 
-		System.out.println("名前を入力してください。");
-		String name = reader.readLine();
-
-		System.out.println("好きなスポーツを入力してください。");
-		String sport = reader.readLine();
-
-		System.out.println("私の名前は" + name + "です。");
-		System.out.println("好きなスポーツは" + sport + "です。");
+		System.out.println("りんごの値段は" + applePrice + "円です。");
+		System.out.println("バナナの値段は" + bananaPrice + "円です。");
+		System.out.println("この筆記用具は" + writingUtensil + "です。");
 
 	}
 
