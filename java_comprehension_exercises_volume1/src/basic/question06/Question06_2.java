2493e7012bb58095374cc88d8e0c0bac7c5db6e8
ykamimu <ykamimu@C010423151.zipangu.cac.co.jp>
2026-04-16 14:09:44 +0900

    Question06完了

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_2.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_2.java
@@ -6,7 +6,11 @@ public class Question06_2 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
-		
+		String[] studentName = { "かなこ", "ゆうた", "あい", "はるひ", "かずき" };
+
+		for (String name : studentName) {
+			System.out.println(name);
+		}
+
 	}
 }
