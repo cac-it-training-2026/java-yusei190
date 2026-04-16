f0afaa0a25cbca345d73671065dd1d615e9a86ba
ykamimu <ykamimu@C010423151.zipangu.cac.co.jp>
2026-04-16 11:02:45 +0900

    Question02完了

--- java_comprehension_exercises_volume1/src/basic/question02/Question02_1.java
+++ java_comprehension_exercises_volume1/src/basic/question02/Question02_1.java
@@ -1,6 +1,8 @@
 package basic.question02;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第5章 入出力
@@ -10,7 +12,17 @@ public class Question02_1 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("名前を入力してください。");
+		String name = reader.readLine();
+
+		System.out.println("好きなスポーツを入力してください。");
+		String sport = reader.readLine();
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("好きなスポーツは" + sport + "です。");
 
 	}
+
 }
--- java_comprehension_exercises_volume1/src/basic/question02/Question02_2.java
+++ java_comprehension_exercises_volume1/src/basic/question02/Question02_2.java
@@ -1,6 +1,8 @@
 package basic.question02;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第5章 入出力
@@ -10,7 +12,18 @@ public class Question02_2 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
+		System.out.println("商品の名前と値段を入力してください。");
+
+		System.out.print("商品の名前>");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String itemName = reader.readLine();
+
+		System.out.print("値段　　　>");
+		String str = reader.readLine();
+		int itemPrice = Integer.parseInt(str);
+
+		System.out.println("商品の名前は" + itemName + "です。");
+		System.out.println("商品の値段は" + itemPrice + "円です。");
 
 	}
 }
