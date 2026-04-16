61a3acbe7068dd41a8bb13627cabf419e8bba338
ykamimu <ykamimu@C010423151.zipangu.cac.co.jp>
2026-04-16 10:29:53 +0900

    Question01_2完了

--- java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
@@ -1,14 +1,27 @@
 package basic.question01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 第4章 変数
  */
 
 public class Question01_2 {
 
-	public static void main(String[] args) {
-		
-		// TODO:ここに実装
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("名前を入力してください。");
+		String name = reader.readLine();
+
+		System.out.println("好きなスポーツを入力してください。");
+		String sport = reader.readLine();
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("好きなスポーツは" + sport + "です。");
 
 	}
 
